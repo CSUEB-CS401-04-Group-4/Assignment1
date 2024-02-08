a73116517729ab6190fe95495ff775d99291b7e8
Marvin Mays <123206246+mmays6@users.noreply.github.com>
2024-02-07 19:24:44 -0800

    Added calculator.java file via upload

---
+++ calculator.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class calculator {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+        String option;
+        
+        do {
+            // Prompt the user to input two numbers
+            System.out.print("Enter the first number: ");
+            int number1 = input.nextInt();
+            System.out.print("Enter the second number: ");
+            int number2 = input.nextInt();
+
+            // Prompt the user to select one of the 4 operations
+            System.out.println("Select an operation:"
+            		+ "\n1. Addition"
+            		+ "\n2. Subtraction"
+            		+ "\n3. Multiplication"
+            		+ "\n4. Division");
+            System.out.print("Enter your choice: ");
+            
+            int operation = input.nextInt();
+
+            double result = 0;
+
+            // Switch case for the selected operation
+            switch (operation) {
+                case 1:
+                    //Todo addition
+                    //code here
+                    break;
+
+                case 2:
+                    //Todo subtraction
+                    //code here
+                    break;
+
+                case 3:
+                    //Todo multiplication
+                    //code here
+                    break;
+
+                case 4:
+                    //Todo division
+                    //code here
+                    break;
+
+                default:
+                    System.out.println("This is not an operation choice!");
+            }
+            // Print result
+            if (operation >= 1 && operation <= 4) {
+                System.out.println("Result: " + result);
+            }
+
+            // Prompt the user on whether or not they want to continue using the calculator
+            System.out.print("Do you want to perform another calculation? (yes/no): ");
+            option = input.next();
+        } while (!option.equals("no"));
+        
+        System.out.println("Goodbye!");
+        input.close();
+		
+	}
+
+}
